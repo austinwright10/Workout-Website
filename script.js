ce1da98629b3909bd25b75e00b401a799b4df421
austinwright10 <austwright10@gmail.com>
2024-03-28 15:48:00 -0600

    dark mode is all completed

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     toggleDisplay('home', 'nav_home');
 });
-// document.addEventListener("DOMContentLoaded", function() {
-//     body.style.backgroundColor = 'white';
-// })
 
 function toggleDisplay(className, className2) {
     //grab all sections in the DOM
@@ -24,7 +21,6 @@ function toggleDisplay(className, className2) {
 document.addEventListener('DOMContentLoaded', function() {
     var darkModeButton = document.getElementById('darkMode');
     darkModeButton.addEventListener('click', function() {
-        console.log('Dark Mode');
         toggleDarkMode();
     });
 });
@@ -32,45 +28,14 @@ document.addEventListener('DOMContentLoaded', function() {
 function toggleDarkMode() {
     var darkModeStylesheet = document.getElementById('darkmode-stylesheet');
     if (darkModeStylesheet) {
-        console.log("if")
+        darkMode.innerHTML = "Click here for Dark Mode!";
         darkModeStylesheet.remove();
     } else {
-        console.log("else")
         var link = document.createElement('link');
         link.id = 'darkmode-stylesheet';
         link.rel = 'stylesheet';
         link.href = 'css/darkmode.css';
         document.head.appendChild(link);
+        darkMode.innerHTML = "Click here for Light Mode!";
     }
 }
-
-
-// function dark_mode() {
-//     const aTags = document.querySelectorAll('a');
-//     const navList = document.querySelector('ul#nav-list');
-//     if (body.style.backgroundColor == 'white') {
-//         body.style.backgroundColor = '#121212';
-//         body.style.color = 'white';
-//         darkMode.innerHTML = 'Click here for Light mode!';
-//         aTags.forEach(tag => {
-//             tag.style.color = 'rgb(0, 100, 255)';
-//         })
-//         nav.style.backgroundColor = '#121212';
-//         nav.style.borderTop = 'solid 2px white'
-//         nav.style.borderBottom = 'solid 2px white'
-//         header.style.backgroundImage = 'linear-gradient(rgb(30, 89, 121), darkblue)'
-//         footer.style.backgroundImage = 'linear-gradient(rgb(30, 89, 121), darkblue)'
-//     } else {
-//         body.style.backgroundColor = 'white';
-//         body.style.color = 'black';
-//         darkMode.innerHTML = 'Click here for Dark mode!';
-//         nav.style.backgroundColor = 'white';
-//         header.style.backgroundImage = 'linear-gradient(rgb(90, 183, 232), lightblue)'
-//         footer.style.backgroundImage = 'linear-gradient(rgb(90, 183, 232), lightblue)'
-//         aTags.forEach(tag => {
-//             tag.style.color = 'black';
-//         })
-//         nav.style.borderTop = 'solid 2px black';
-//         nav.style.borderBottom = 'solid 2px black';
-//     }
-// }
